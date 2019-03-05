3200cbd050196bafc60cad2a0aa2d2ed29ed7a14
Anastasiya <natay1304@gmail.com>
2019-03-05 17:46:28 +0300

    Добавьте файлы проекта.

---
+++ C_Sharp_Light7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Light7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
